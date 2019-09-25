47af782290130aaf66b50f28a257319f0a39dfb4
shengalingdork <scamus@CUP-MNL-D196>
2019-09-25 16:56:14 +0800

    fix footer and note generation format

--- resources/views/layouts/elements/footer.blade.php
+++ resources/views/layouts/elements/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer" style="margin-top:50px">
     <div class="fixed-bottom">
-        <p class="text-muted text-center">&copy; {{ now()->year }} Shane Camus</p>
+        <p class="text-muted text-center">&copy; {{ now()->year }} Platform Education Technology</p>
     </div>
 </footer>
